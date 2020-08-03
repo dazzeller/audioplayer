25e4f22d371d5cd7bc187194c9d5a88b986daab8
dazzeller <51502865+dazzeller@users.noreply.github.com>
2020-08-03 21:49:59 +0530

    main.dart

---
+++ audioplayer task/lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+typedef void OnError(Exception exception);
+
+void main() {
+  runApp(new MaterialApp(debugShowCheckedModeBanner: false,home:  LocalAudio()));
+}
+_launchURL() async {
+  const url = 'https://gaana.com/playlist/tanmay5709-gannacom';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+
+class LocalAudio extends StatefulWidget {
+  @override
+  _LocalAudio createState() =>  _LocalAudio();
+}
+
+class _LocalAudio extends State<LocalAudio> {
+  Duration _duration = new Duration();
+  Duration _position = new Duration();
+  AudioPlayer advancedPlayer;
+  AudioCache audioCache;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
+  }
+
+  void initPlayer() {
+    advancedPlayer = new AudioPlayer();
+    audioCache = new AudioCache(fixedPlayer: advancedPlayer);
+
+    advancedPlayer.durationHandler = (d) => setState(() {
+          _duration = d;
+        });
+
+    advancedPlayer.positionHandler = (p) => setState(() {
+          _position = p;
+        });
+  }
+
+  String localFilePath;
+
+  Widget _tab(List<Widget> children) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: children
+               .map((w) => Container(child: w, padding: EdgeInsets.all(6.0)))
+               .toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _btn(String txt, VoidCallback onPressed) {
+    return ButtonTheme(
+      minWidth: 48.0,
+      child: Container(
+        width: 150,
+        height: 45,
+        child: RaisedButton(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            child: Text(txt),
+            color: Colors.purple[200],
+            textColor: Colors.white,
+            onPressed: onPressed),
+      ),
+    );
+  }
+
+  Widget slider() {
+    return Slider(
+        activeColor: Colors.black,
+        inactiveColor: Colors.purple,
+        value: _position.inSeconds.toDouble(),
+        min: 0.0,
+        max: _duration.inSeconds.toDouble(),
+        onChanged: (double value) {
+          setState(() {
+            seekToSecond(value.toInt());
+            value = value;
+          });
+        });
+  }
+
+  Widget LocalAudio() {
+    return _tab([
+      _btn('Play', () => audioCache.play('soft.mp3')),
+      _btn('Pause', () => advancedPlayer.pause()),
+      _btn('Stop', () => advancedPlayer.stop()),
+      slider()
+    ]);
+  }
+
+  void seekToSecond(int second) {
+    Duration newDuration = Duration(seconds: second);
+
+    advancedPlayer.seek(newDuration);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(     // for enabling the control over the tabs
+      length: 1,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _launchURL,
+          label: Text("ganna.com"),
+        
+          icon: Icon(Icons.music_note),
+          ),
+      
+     
+      
+      
+        backgroundColor: Colors.white,
+      
+        appBar: AppBar(
+          elevation: 1.0,
+          backgroundColor: Colors.blue,
+          title: Center(child: Text('AUDIO  PLAYER')),
+        ),
+        body: TabBarView(
+        
+          children: [LocalAudio()],
+        ),
+      
+      ),
+    );
+  }
+}
